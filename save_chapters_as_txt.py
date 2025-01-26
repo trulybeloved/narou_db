12c27413af6c8a68b5174b50b57eb26d8ae079f4
Beloved <toshiroakari@gmail.com>
2025-01-26 18:02:19 +0000

    wip: repo management

--- save_chapters_as_txt.py
+++ save_chapters_as_txt.py
@@ -3,10 +3,7 @@ from bs4 import BeautifulSoup
 import custom_modules.utilities as utils
 import custom_modules.narou_parser as narou_parser
 
-with open('datastores/chapter_scrape_resuslt.json', 'r', encoding='utf-8') as chapter_scrape_results_json:
-    chapter_scrape_results = json.loads(chapter_scrape_results_json.read())
-
-for chapter in chapter_scrape_results:
+def process_chapter_for_save(chapter_scrape_result):
 
     narou_uid = narou_parser.get_narou_uid_from_url(chapter['scraped_url'])
     narou_uid = str(narou_uid).zfill(5)
@@ -17,6 +14,48 @@ for chapter in chapter_scrape_results:
 
     chapter_text = f'{chapter_title_soup.text}\n{chapter_soup.text}'
     chapter_html = f'{chapter_title_soup.prettify()}\n{chapter_soup.prettify()}'
+
+    for tag in chapter_soup.find_all(True):
+        if tag.name not in ['ruby', 'rt', 'rp']:
+            tag.unwrap()
+
+    chapter_text = f'{chapter_title_soup.text}\n{chapter_soup.prettify()}'
+
+    chapter_title_parse_results = narou_parser.parse_chapter_title(chapter_title_soup.text)
+    print(chapter_title_parse_results)
+
+    return chapter_text, chapter_html
+
+def save_chapter_text_html(chapter_text, chapter_html, narou_uid, chapter_text_datastore = 'datastore/chapter_raws_txt'):
+
+    with open(f'datastores/chapter_raws_txt/{narou_uid}.txt', 'w', encoding='utf-8') as raw_text_file:
+        raw_text_file.write(chapter_text)
+
+    with open(f'datastores/chapter_raws_html/{narou_uid}.html' , 'w', encoding='utf-8') as raw_html_file:
+        raw_html_file.write(chapter_html)
+
+
+
+with open('datastores/chapter_scrape_resuslt.json', 'r', encoding='utf-8') as chapter_scrape_results_json:
+    chapter_scrape_results = json.loads(chapter_scrape_results_json.read())
+
+for chapter in chapter_scrape_results[130:]:
+
+    narou_uid = narou_parser.get_narou_uid_from_url(chapter['scraped_url'])
+    narou_uid = str(narou_uid).zfill(5)
+    print(narou_uid)
+
+    chapter_title_soup = BeautifulSoup(chapter['scrape_results']['.p-novel__title'], 'html.parser')
+    chapter_soup = BeautifulSoup(chapter['scrape_results']['.p-novel__body'], 'html.parser')
+
+    # chapter_text = f'{chapter_title_soup.text}\n{chapter_soup.text}'
+    chapter_html = f'{chapter_title_soup.prettify()}\n{chapter_soup.prettify()}'
+
+    for tag in chapter_soup.find_all(True):
+        if tag.name not in ['ruby', 'rt', 'rp']:
+            tag.unwrap()
+
+    chapter_text = f'{chapter_title_soup.text}\n{chapter_soup}'
 
     chapter_title_parse_results = narou_parser.parse_chapter_title(chapter_title_soup.text)
     print(chapter_title_parse_results)
